6d62c2f18c99f7ba28e1de41fa4de7fb8ae085a5
Monkeluffy <vlad.kvartal@yandex.ru>
2023-01-26 09:30:49 +0300

    ЗАДАНИЕ 18.8.19

--- script.py
+++ script.py
@@ -1,7 +1,31 @@
-per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-money = int(input('Введите сумму вклада:'))
-deposit = []
-for key in per_cent: deposit.append(int(per_cent[key] * money / 100))
-max_deposit = max(deposit)
-print(deposit)
-print('Максимальная сумма, которорую вы можете заработать - ' + str(max_deposit) + ' руб.')
+tickets = int(input('Введите количество билетов: '))
+
+
+ages = []
+
+
+for i in range(0, tickets):
+    input_value = int(input(f'Введите возраст {i + 1} участника: '))
+    ages.append(input_value)
+
+
+    def ticket_prise(ages):
+        if ages < 18:
+            return 0
+        elif 18 <= ages < 25:
+            return 990
+        elif 25 <= ages:
+            return 1390
+
+
+    total_ = sum(map(ticket_prise, ages))
+
+
+discount = int(total_ * 0.1)
+discounted_prise = int(total_ - discount)
+if tickets >= 3:
+    print('Полная стоимость билетов: ', total_, "руб.")
+    print('Сумма скидки (при регистрации более 3 участников предоставляется скидка 10%): ', discount, "руб.")
+    print('Общая стоимость билетов со скидкой: ', discounted_prise, "руб.")
+else:
+    print('Общая стоимость билетов: ', total_, "руб.")
